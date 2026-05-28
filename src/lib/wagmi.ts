fdec9b496e6ddeb4c32fa1b6c4c066a042d08a67
mksdznk <maks.deznak@gmail.com>
2026-05-28 22:56:04 +0200

    first commit

---
+++ src/lib/wagmi.ts
@@ -0,0 +1,15 @@
+import { http, createConfig } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { injected, walletConnect } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [mainnet, sepolia],
+  connectors: [
+    injected(),
+    walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? "" }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
+});
